99eeb94879443bb05569044dbf4774312dcbcf32
djj1111 <dengjiajun1108@gmail.com>
2016-12-05 23:59:40 +0800

    20161206

--- app/src/main/java/com/djj/example/wocao/MyFragment.java
+++ app/src/main/java/com/djj/example/wocao/MyFragment.java
@@ -3,10 +3,12 @@ package com.djj.example.wocao;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.EditText;
 
 /**
  * Created by djj on 2016/12/4.
@@ -18,7 +20,7 @@ public class MyFragment extends Fragment {
     public final static String SaveInstanceStateEXTRA = "SaveInstanceStateEXTRA";
 
     //String mData;
-    TestTable mTestTable;
+    private TestTable mTestTable;
 
     public static MyFragment getInstances(TestTable t) {
         MyFragment myFragment = new MyFragment();
@@ -41,10 +43,68 @@ public class MyFragment extends Fragment {
 
         // 这里必须是null
         View item = inflater.inflate(R.layout.fragment_item, null);
-        TextView textid = (TextView) item.findViewById(R.id.tvid);
-        TextView texttext = (TextView) item.findViewById(R.id.tvtext);
-        TextView textip = (TextView) item.findViewById(R.id.tvip);
-        TextView texttime = (TextView) item.findViewById(R.id.tvtime);
+        EditText textid = (EditText) item.findViewById(R.id.tvid);
+        EditText texttext = (EditText) item.findViewById(R.id.tvtext);
+        EditText textip = (EditText) item.findViewById(R.id.tvip);
+        EditText texttime = (EditText) item.findViewById(R.id.tvtime);
+        textid.setEnabled(false);
+        texttext.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before,
+                                      int count) {
+//
+                mTestTable.setName(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+        });
+        textip.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before,
+                                      int count) {
+//
+                mTestTable.setAddress(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+        });
+        texttime.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before,
+                                      int count) {
+//
+                mTestTable.setPhone(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+        });
         textid.setText(String.valueOf(mTestTable.getId()));
         texttext.setText(mTestTable.getName());
         textip.setText(mTestTable.getAddress());
@@ -70,6 +130,10 @@ public class MyFragment extends Fragment {
     public void onDestroy() {
         super.onDestroy();
        // Log.d(TAG, "onDestroy mData = " + mData);
+    }
+
+    public TestTable getmTestTable(){
+        return mTestTable;
     }
 
 }
--- app/src/main/java/com/djj/example/wocao/MyFragmentActivity.java
+++ app/src/main/java/com/djj/example/wocao/MyFragmentActivity.java
@@ -3,10 +3,11 @@ package com.djj.example.wocao;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
-import android.util.Log;
+import android.view.View;
 
 import org.xutils.DbManager;
 import org.xutils.ex.DbException;
+import org.xutils.view.annotation.Event;
 import org.xutils.x;
 
 import java.util.ArrayList;
@@ -15,36 +16,90 @@ import java.util.ArrayList;
  * Created by djj on 2016/12/4.
  */
 
+
 public class MyFragmentActivity extends FragmentActivity {
+
     private ArrayList<TestTable> tablelist;
     private DbManager db;
     private MyFragmentStatePagerAdapter mFragmentStatePagerAdapter;
+    private ViewPager mViewPager;
+    private void add(){
+        TestTable table = new TestTable();
+
+        try {
+            db.saveBindingId(table);
+        } catch (DbException e) {
+            e.printStackTrace();
+        }
+        tablelist.add(table);
+        MyFragment fragment=MyFragment.getInstances(table);
+        mFragmentStatePagerAdapter.addFragment(fragment);
+        int position=mFragmentStatePagerAdapter.getItemPosition(fragment);
+        mViewPager.setCurrentItem(position);
+    }
+    private void initshow(){
+        if (tablelist.isEmpty()) {add();}
+        else{
+            for (TestTable t : tablelist){
+                mFragmentStatePagerAdapter.addFragment(MyFragment.getInstances(t));
+            }
+        }
+
+    }
+    private void delete(){
+        MyFragment fragment=(MyFragment) mFragmentStatePagerAdapter.getItem(mViewPager.getCurrentItem());
+        TestTable t= fragment.getmTestTable();
+        if (t!=null) {
+            try {
+                db.delete(t);
+            } catch (DbException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Event(value = R.id.button_add, type = View.OnClickListener.class)
+    private void bt1Click(View v) {
+        add();
+    }
+    @Event(value = R.id.button_delete, type = View.OnClickListener.class)
+    private void bt2Click(View v) {
+        delete();
+    }
+    @Event(value = R.id.button_save, type = View.OnClickListener.class)
+    private void bt3Click(View v) {
+        for(TestTable t : tablelist){
+            try {
+                db.update(t, "name","address","phone");
+            } catch (DbException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_main);
-        //x.view().inject(this);
+        x.view().inject(this);
         mFragmentStatePagerAdapter=new MyFragmentStatePagerAdapter(getSupportFragmentManager());
-        ViewPager mViewPager=(ViewPager) findViewById(R.id.viewpager);
-        Log.d("fuck","asdfasdfasdfasfdasfafda");
+        mViewPager=(ViewPager) findViewById(R.id.viewpager);
+       // Log.d("fuck","asdfasdfasdfasfdasfafda");
         db_init();
-        tablelist=new ArrayList<>();
-        if (tablelist.isEmpty()){
+        //tablelist=new ArrayList<>();
+        /*if (tablelist.isEmpty()){
            TestTable t=new TestTable();
             t.setId(3);
             t.setPhone("38373893");
             t.setAddress("addr");
             t.setName("hu");
             tablelist.add(t);
-        }
-        Log.d("fuck","asdfasdfasdfasfdasfafdaassss");
-            for (TestTable t : tablelist){
-                mFragmentStatePagerAdapter.addFragment(MyFragment.getInstances(t));
-            }
+        }*/
+        //Log.d("fuck","asdfasdfasdfasfdasfafdaassss");
 
-
+        initshow();
         mViewPager.setAdapter(mFragmentStatePagerAdapter);
-
     }
 
     private void db_init(){
